24f48b4e01560d409c1e3992a99eef47f0a4c8d9
Felix Wednesday <felix.dido.4@gmail.com>
2023-06-12 17:26:46 +0400

    removed unneccesary code

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,5 @@
 var cacheName = "appStore-v1";
 
-// self.addEventListener("install", (e) => {
-//   console.log("[service worker] Install");
-//   e.waitUntill(
-//     caches.open(cacheName).then((cache) => {
-//       console.log("[Service Worker] Caching all files");
-//       return cache.addAll(cacheFiles);
-//     })
-//   );
-// });
-
-// //intercepts requests to retrun cached files
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     caches.match(e.request).then((r) => {
-//       console.log("[service worker] fetching resource:" + e.request.url);
-//       return r;
-//     })
-//   );
-// });
-
 //intercepts requests to save all fetched files in the cache if new or return from cache if exisiting
 self.addEventListener("fetch", (e) => {
   e.respondWith(
