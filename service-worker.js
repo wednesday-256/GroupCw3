c0601963b9839b750fa32efa98e2d0dcb9d1787d
Felix Wednesday <felix.dido.4@gmail.com>
2023-06-12 17:39:01 +0400

    added comments to service worker file

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,9 @@ var cacheName = "appStore-v1";
 //intercepts requests to save all fetched files in the cache if new or return from cache if exisiting
 self.addEventListener("fetch", (e) => {
   e.respondWith(
+    // checks if the requested file is cached
     caches.match(e.request).then((r) => {
+      // returns the caches file or requests it if not available
       return (
         r ||
         fetch(e.request).then((response) => {
